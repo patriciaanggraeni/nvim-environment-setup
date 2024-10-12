1ac7f7a6c4bef693ae9be0331fec019c1564e3da
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2024-10-12 12:36:51 +0700

    treesittet v2

--- lua/plugins/global/treesitter.lua
+++ lua/plugins/global/treesitter.lua
@@ -1,5 +1,12 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { "java", "kotlin" },
+    ensure_installed = { 
+        "javascript", "typescript", "java", "kotlin", 
+        "html", "css", "scss", "vue", "python", "dart",
+        "json", "yaml", "xml", "vue", "angular", "tsx",
+        "markdown", "markdown_inline", "php", "sql", 
+        "powershell", "gitignore", "gitcommit", "gitattributes",
+        "git_rebase", "git_config"
+    },
     sync_install = false,
     auto_install = true,
     highlight = {
